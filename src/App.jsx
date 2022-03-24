c033c5ecb19e12e96515e2c31a54b9f432295c97
Luis Zunino <zuninoluis01@gmail.com>
2022-03-24 16:14:41 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import NavegationBar from "./components/NavegationBar";
 import HomePage from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
+      <NavegationBar />
       <HomePage />
+
+      <Footer />
     </div>
   );
 }
