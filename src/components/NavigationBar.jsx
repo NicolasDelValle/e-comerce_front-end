3d7d859db09a1f86e6a7e095fa3417289a073cd1
Luis Zunino <zuninoluis01@gmail.com>
2022-03-24 20:32:41 -0300

    se modifica a un submenu de bootstrap

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import "./css/NavigationBar.css";
 import { useState } from "react";
+import { Dropdown } from "react-bootstrap";
+import "./css/NavigationBar.css";
 import logo from "../img/logos/imagotipoNegro.png";
 
 function NavigationBar() {
@@ -9,38 +10,45 @@ function NavigationBar() {
   return (
     <>
       <div className="navigation">
-        <div className="container">
-          <div className="bg-transparent p-3 d-flex justify-content-between apgn-items-center">
-            <div
-              className="d-flex align-items-center"
-              onClick={() => {
-                setOpen((prev) => !prev);
-              }}
+        <div className="container d-flex">
+          <Dropdown className="w-100">
+            <Dropdown.Toggle
+              className="bg-transparent d-flex justify-content-between align-items-center btnMenu "
+              title="Boton de inicio"
             >
-              <div className="btnMenu" title="Boton de inicio">
-                <img src={`${logo}`} alt="Logo" />
-              </div>
+              <img src={`${logo}`} alt="Logo" />
               <h2 className="ps-2 btn text-white d-none d-sm-block">Inicio</h2>
-            </div>
-            <div
-              className="btnCarrito d-flex align-items-center"
-              title="Carrito de compras"
-            >
-              <i className="bi bi-cart3 pe-2"></i>
-            </div>
-          </div>
-          <div className={open ? "abierto " : "cerrado"}>
-            <div className="row">
-              <div className="col-12 col-md-4">
-                <p className="subMenu-hijos">Home</p>
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="">
+              <div className="row">
+                <div className="col-md-3">
+                  <Dropdown.Item href="#/action-1">
+                    <div className="px-2">Home</div>
+                  </Dropdown.Item>
+                </div>
+                <div className="col-md-3">
+                  <Dropdown.Item href="#/action-1">
+                    <div className="px-2">Productos</div>
+                  </Dropdown.Item>
+                </div>
+                <div className="col-md-3">
+                  <Dropdown.Item href="#/action-1">
+                    <div className="px-2">Sobre el proyecto</div>
+                  </Dropdown.Item>
+                </div>
+                <div className="col-md-3">
+                  <Dropdown.Item href="#/action-1">
+                    <div className="px-2">Sobre el Nosotros</div>
+                  </Dropdown.Item>
+                </div>
               </div>
-              <div className="col-12 col-md-4">
-                <p className="subMenu-hijos">Productos</p>
-              </div>
-              <div className="col-12 col-md-4">
-                <p className="subMenu-hijos">Sobre el proyecto</p>
-              </div>
-            </div>
+            </Dropdown.Menu>
+          </Dropdown>
+          <div
+            className="btnCarrito d-flex align-items-center ms-auto"
+            title="Carrito de compras"
+          >
+            <i className="bi bi-cart3 pe-2"></i>
           </div>
         </div>
       </div>
