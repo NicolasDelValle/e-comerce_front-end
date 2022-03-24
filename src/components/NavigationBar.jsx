3dae15e51cac8615d90a64b65b7624b1c03ffc58
Luis Zunino <zuninoluis01@gmail.com>
2022-03-24 19:34:49 -0300

    se cambia a row y col

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -8,29 +8,40 @@ function NavigationBar() {
 
   return (
     <>
-      <div className="nav">
-        <div className="position-relative container bg-transparent p-3 d-flex justify-content-between align-items-center">
-          <div
-            className="d-flex align-items-center"
-            onClick={() => {
-              setOpen((prev) => !prev);
-            }}
-          >
-            <div className="btnMenu">
-              <img src={`${logo}`} alt="Logo" />
+      <div className="navigation">
+        <div className="container">
+          <div className="bg-transparent p-3 d-flex justify-content-between apgn-items-center">
+            <div
+              className="d-flex align-items-center"
+              onClick={() => {
+                setOpen((prev) => !prev);
+              }}
+            >
+              <div className="btnMenu" title="Boton de inicio">
+                <img src={`${logo}`} alt="Logo" />
+              </div>
+              <h2 className="ps-2 btn text-white d-none d-sm-block">Inicio</h2>
             </div>
-            <h2 className="ps-2 btn text-white">Inicio</h2>
+            <div
+              className="btnCarrito d-flex align-items-center"
+              title="Carrito de compras"
+            >
+              <i className="bi bi-cart3 pe-2"></i>
+            </div>
           </div>
-          <div className="">
-            <i className="bi bi-cart3"></i>
+          <div className={open ? "abierto " : "cerrado"}>
+            <div className="row">
+              <div className="col-12 col-md-4">
+                <p className="subMenu-hijos">Home</p>
+              </div>
+              <div className="col-12 col-md-4">
+                <p className="subMenu-hijos">Productos</p>
+              </div>
+              <div className="col-12 col-md-4">
+                <p className="subMenu-hijos">Sobre el proyecto</p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className={open ? "abierto " : "cerrado"}>
-          <ul className="subMenu">
-            <li className="subMenu-hijos">Home</li>
-            <li className="subMenu-hijos">Productos</li>
-            <li className="subMenu-hijos">Sobre el proyecto</li>
-          </ul>
         </div>
       </div>
     </>
