91e3fa6cbe207c612db6e8ccaf7e72d625731fed
Luis Zunino <zuninoluis01@gmail.com>
2022-03-25 15:33:07 -0300

    update home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,59 +5,64 @@ import prod1 from "../../img/productos/tablas/tabla.png";
 import prod2 from "../../img/productos/tablas/tabla2.png";
 import prod3 from "../../img/productos/tablas/tabla3color.png";
 import Footer from "../../components/Footer";
+import NavigationBar from "../../components/NavigationBar";
 
 function Home() {
   return (
-    <div className="homeScrollContainer ">
-      <section
-        className="homeScrollContainer-seccion "
-        style={{
-          backgroundImage: "url(https://wallpaper.dog/large/5450544.jpg)",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <div>
-          <button>
-            hola
-            <i className="bi bi-caret-down-fill"></i>
-          </button>
-        </div>
-      </section>
-      <section className="homeScrollContainer-seccion">
-        <Container>
+    <>
+      <NavigationBar />
+      <div className="homeScrollContainer ">
+        <section
+          className="homeScrollContainer-seccion "
+          style={{
+            backgroundImage: "url(https://wallpaper.dog/large/5450544.jpg)",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
           <div>
-            <div className="border-bottom border-2">
-              <h2>
-                <span>Shop</span>
-              </h2>
-            </div>
-            <Row>
-              <Col className="productCard m-3 py-3">
-                <img width="120px" height="440" src={prod1} alt="tabla 1" />
-              </Col>
-              <Col className="productCard m-3 py-3 rounded-circle">
-                <img width="120px" height="440" src={prod2} alt="tyabla 2" />
-              </Col>
-              <Col className="productCard m-3 py-3 rounded-circle">
-                <img width="120px" height="440" src={prod3} alt="tabla 3" />
-              </Col>
-            </Row>
-            <Row>
-              <Col className="productCard m-3 py-3 ">
-                <img width="120px" height="440" src={prod1} alt="tabla 1" />
-              </Col>
-              <Col className="productCard m-3 py-3 ">
-                <img width="120px" height="440" src={prod2} alt="tyabla 2" />
-              </Col>
-              <Col className="productCard m-3 py-3 ">
-                <img width="120px" height="440" src={prod3} alt="tabla 3" />
-              </Col>
-            </Row>
+            <button>
+              hola
+              <i className="bi bi-caret-down-fill"></i>
+            </button>
           </div>
-        </Container>
-      </section>
-    </div>
+        </section>
+        <section className="homeScrollContainer-seccion">
+          <Container>
+            <div>
+              <div className="border-bottom border-2">
+                <h2>
+                  <span>Shop</span>
+                </h2>
+              </div>
+              <Row>
+                <Col className="productCard m-3 py-3">
+                  <img width="120px" height="440" src={prod1} alt="tabla 1" />
+                </Col>
+                <Col className="productCard m-3 py-3 rounded-circle">
+                  <img width="120px" height="440" src={prod2} alt="tyabla 2" />
+                </Col>
+                <Col className="productCard m-3 py-3 rounded-circle">
+                  <img width="120px" height="440" src={prod3} alt="tabla 3" />
+                </Col>
+              </Row>
+              <Row>
+                <Col className="productCard m-3 py-3 ">
+                  <img width="120px" height="440" src={prod1} alt="tabla 1" />
+                </Col>
+                <Col className="productCard m-3 py-3 ">
+                  <img width="120px" height="440" src={prod2} alt="tyabla 2" />
+                </Col>
+                <Col className="productCard m-3 py-3 ">
+                  <img width="120px" height="440" src={prod3} alt="tabla 3" />
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </section>
+      </div>
+      <Footer />
+    </>
   );
 }
 
