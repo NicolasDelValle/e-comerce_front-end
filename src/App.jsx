30b28d182784b87139f3f47a375ad14ab260e5c0
Luis Zunino <zuninoluis01@gmail.com>
2022-03-25 11:51:01 -0300

    rutas y estilos css del nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Shop from "./pages/Shop";
+import Home from "./pages/home/Home";
+import Shop from "./pages/shop/Shop";
+import AboutUs from "./components/AboutUs";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/products/:id" element={<Shop />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/aboutproject" element={<AboutUs />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
   );
