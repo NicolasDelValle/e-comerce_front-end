cabc20971cfcac3404af29f26c462636a35e2f59
mauroraviolo23 <mauro.raviolo@gmail.com>
2022-03-29 01:41:07 -0300

    .

--- src/pages/checkOut/CheckOut.jsx
+++ src/pages/checkOut/CheckOut.jsx
@@ -3,52 +3,6 @@ import Footer from "../../components/Footer";
 import NavigationBar from "../../components/NavigationBar";
 import "./checkOut.css";
 import CartItem from "../../components/CartItem";
-import { Accordion } from "react-bootstrap";
-import { Card } from "react-bootstrap";
-
-function CheckOut() {
-  function CustomToggle({ children, eventKey }) {
-    const decoratedOnClick = React.useAccordionButton(eventKey, () =>
-      console.log("totally custom!")
-    );
-
-    return (
-      <button
-        type="button"
-        style={{ backgroundColor: "pink" }}
-        onClick={decoratedOnClick}
-      >
-        {children}
-      </button>
-    );
-  }
-
-  function Example() {
-    return (
-      <Accordion defaultActiveKey="0">
-        <Card>
-          <Card.Header>
-            <CustomToggle eventKey="0">Click me!</CustomToggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>Hello! I'm the body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <CustomToggle eventKey="1">Click me!</CustomToggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm another body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
-    );
-  }
-
-  // render(<Example />);
-
-  const [open, setOpen] = React.useState(false);
 
 function CheckOut() {
 
