d57f22caaa475c1cca774d90edca66e0633398b4
Luis Zunino <zuninoluis01@gmail.com>
2022-03-30 17:38:17 -0300

    se agrega enlace al componente productCard para que rediriga

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import AddCart from "./AddCart";
 import "./css/Product.css";
 function ProductCard({ product }) {
   return (
     <>
-      <Col className="productCard m-3 py-3">
-        <img
-          className="d-block mx-auto"
-          width="120px"
-          height="440"
-          src={product.imageUrl}
-          alt={product.name}
-        />
-        <p className="product-info">{product.name}</p>
-        <p className="product-info">${product.price}</p>
-        <AddCart />
+      <Col className="productCard">
+        <Link to={`/products/${product.slug}`}>
+          <div className="card p-1">
+            <span className="py-2">
+              <img
+                className="w-auto mb-2"
+                height="220px"
+                src={product.imageUrl}
+                alt={product.name}
+              />
+              <p className="product-info">{product.name}</p>
+              <p className="product-info">${product.price}</p>
+              <AddCart />
+            </span>
+          </div>
+        </Link>
       </Col>
     </>
   );
