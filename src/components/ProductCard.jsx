9e107ba8dba89e193a2664aaf04c8de54c053e76
Luis Zunino <zuninoluis01@gmail.com>
2022-03-30 17:49:23 -0300

    se hacen modificaciones de css

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import AddCart from "./AddCart";
-import "./css/Product.css";
+import "./css/ProductCard.css";
 function ProductCard({ product }) {
   return (
     <>
       <Col className="productCard">
         <Link to={`/products/${product.slug}`}>
-          <div className="card p-1">
+          <div className="card p-1 text-black">
             <span className="py-2">
               <img
                 className="w-auto mb-2"
@@ -16,8 +16,12 @@ function ProductCard({ product }) {
                 src={product.imageUrl}
                 alt={product.name}
               />
-              <p className="product-info">{product.name}</p>
-              <p className="product-info">${product.price}</p>
+              <p className="product-info">
+                <strong>{product.name}</strong>
+              </p>
+              <p className="product-info">
+                <strong>${product.price}</strong>
+              </p>
               <AddCart />
             </span>
           </div>
