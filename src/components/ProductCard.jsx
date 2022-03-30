e4e87a5bbcdcb765bf2662873ec586b29d008681
Luis Zunino <zuninoluis01@gmail.com>
2022-03-30 17:58:19 -0300

    se coloca spinner de carga

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,28 @@
-import React from "react";
-import { Col } from "react-bootstrap";
+import { useState } from "react";
+import { Col, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import AddCart from "./AddCart";
 import "./css/ProductCard.css";
 function ProductCard({ product }) {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <>
       <Col className="productCard">
+        {isLoading && (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        )}
         <Link to={`/products/${product.slug}`}>
           <div className="card p-1 text-black">
             <span className="py-2">
               <img
-                className="w-auto mb-2"
+                className={isLoading ? "d-none" : "w-auto mb-2"}
                 height="220px"
                 src={product.imageUrl}
                 alt={product.name}
+                onLoad={() => setIsLoading(false)}
               />
               <p className="product-info">
                 <strong>{product.name}</strong>
