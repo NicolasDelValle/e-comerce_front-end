af3455ed00de0593d5721be78a045b8e28ec0d01
Luis Zunino <zuninoluis01@gmail.com>
2022-03-31 18:47:44 -0300

    se agrega la barra que regresa al home

--- src/pages/admin/AdminProducts.jsx
+++ src/pages/admin/AdminProducts.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
-import { Button, Modal } from "react-bootstrap";
-import { Link } from "react-router-dom";
+// import { Button } from "react-bootstrap";
+// import { Link } from "react-router-dom";
 import axios from "axios";
 import { useSelector } from "react-redux";
-import ModalNewProduct from "./ModalNewProduct";
+
 import AdminProductsItem from "./AdminProductsItem";
 import Sidebar from "../../components/Sidebar";
-
+import BackNavbar from "../../components/BackNavbar";
 function AdminProducts({ show, handleClose }) {
   const newToken = useSelector((state) => state.user.newToken);
 
@@ -28,28 +28,32 @@ function AdminProducts({ show, handleClose }) {
   return (
     <div className="d-flex">
       <Sidebar />
-
-      <div className="container-fluid px-4 w-75  mx-auto py-5">
-        <h3>Lista de Productos</h3>
-        <div className="row mt-4">
-          <div className="table-responsive">
-            <table className="table">
-              <thead className="table-light">
-                <tr>
-                  <th scope="col" onClick={() => setProducts()}>
-                    Nombre
-                  </th>
-                  <th scope="col">Precio</th>
-                  <th scope="col">Destacado</th>
-                  <th scope="col">Stock</th>
-                  <th scope="col">Administrar</th>
-                </tr>
-              </thead>
-              <tbody>
-                {products.length > 0 &&
-                  products.map((item) => <AdminProductsItem item={item} />)}
-              </tbody>
-            </table>
+      <div className="w-100">
+        <BackNavbar />
+        <div className="container-fluid px-4 w-75  mx-auto py-5">
+          <h3>Lista de Productos</h3>
+          <div className="row mt-4">
+            <div className="table-responsive">
+              <table className="table">
+                <thead className="table-light">
+                  <tr>
+                    <th scope="col" onClick={() => setProducts()}>
+                      Nombre
+                    </th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Destacado</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Administrar</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.length > 0 &&
+                    products.map((item) => (
+                      <AdminProductsItem key={item.slug} item={item} />
+                    ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
