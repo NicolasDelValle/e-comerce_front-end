e414fc076809e71cf153414173f1b67e4dc6716d
Luis Zunino <zuninoluis01@gmail.com>
2022-03-30 22:27:20 -0300

    se agrega linea de autorizacion

--- src/pages/admin/AdminProducts.jsx
+++ src/pages/admin/AdminProducts.jsx
@@ -4,52 +4,55 @@ import axios from "axios";
 
 import "./admin.css";
 import BackNavbar from "../../components/BackNavbar";
+import { useSelector } from "react-redux";
 
 function AdminProducts() {
-	const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const token = useSelector((state) => state.user.token);
 
-	useEffect(() => {
-		const getProducts = async () => {
-			const { data } = await axios({
-				method: "get",
-				url: `${process.env.REACT_APP_API_URL}products`,
-			});
-			console.log(data);
-			setProducts(data);
-		};
-		getProducts();
-	}, []);
+  useEffect(() => {
+    const getProducts = async () => {
+      const { data } = await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}products`,
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log(data);
+      setProducts(data);
+    };
+    getProducts();
+  }, []);
 
-	return (
-		<>
-			<div className="admin-main-content">
-				<div className="admin-main-content-options">
-					<button className="btn btn-primary">Nuevo Producto</button>
-				</div>
-				<div className="admin-main-content-data">
-					<table className="table">
-						<thead>
-							<tr>
-								<th scope="col">Nombre</th>
-								<th scope="col">Precio</th>
-								<th scope="col">Stock</th>
-							</tr>
-						</thead>
-						<tbody>
-							{products.length > 0 &&
-								products.map((item) => (
-									<tr key={item.id}>
-										<td>{item.name}</td>
-										<td>{item.price}</td>
-										<td>{item.stock}</td>
-									</tr>
-								))}
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="admin-main-content">
+        <div className="admin-main-content-options">
+          <button className="btn btn-primary">Nuevo Producto</button>
+        </div>
+        <div className="admin-main-content-data">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Stock</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.length > 0 &&
+                products.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <td>{item.stock}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AdminProducts;
