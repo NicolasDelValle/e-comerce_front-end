34bf1503a3d3b01ac11733310d8f431ec3b61825
Charlygaf <charlygaf@gmail.com>
2022-03-31 11:46:11 -0300

    product page home button verr detalles

---
+++ src/components/DetailsButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function DetailsButton() {
+  return (
+    <button
+      className="my-3 btn bg-black text-white w-auto rounded-pill"
+      title="Agregar al carrito"
+    >
+      <p className="p-0 m-0">Ver producto</p>
+    </button>
+  );
+}
+
+export default DetailsButton;
