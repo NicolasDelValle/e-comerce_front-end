b1d3d3c2a699c345944e99d66759eeb04785df2f
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-03-30 23:54:26 -0300

    Mejoras a cartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,39 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export const CartItem = (props) => {
+export const CartItem = ({ url, price, mountProp, name, stock }) => {
+  const [mount, setMount] = useState(mountProp);
+
+  const handleIncrement = () => {
+    setMount(Number(mount) + 1);
+  };
+  const handleDecrement = () => {
+    Number(mount) === 0 ? setMount(0) : setMount(Number(mount) - 1);
+  };
+
   return (
     <div className="d-flex flex-row align-items-center w-100 mb-3">
       <div
         className="item-image rounded me-2"
         style={{
-          backgroundImage:
-            "url(https://cybmmybwfbmxasdepuyj.supabase.co/storage/v1/object/public/images/hardware/tornillos.png)",
+          backgroundImage: `url(${url})`,
         }}
       ></div>
       <div className="w-100">
         <div className="w-100 d-flex flex-row align-items-start mb-1">
-          <span className="w-75 text-start overflow-hidden">
-            NOMBRE RE GOD CON LA PATINETA DE LO DIOSE
+          <span className="w-75 text-start overflow-hidden">{name}</span>
+          <span className="ms-auto fw-bold">
+            ${Number(price) * Number(mount)}
           </span>
-          <span className="ms-auto fw-bold">$3123123123</span>
         </div>
-        <div className="d-flex flex-row align-items-start text-secondary fw-light mb-1">
+        <div className="d-flex flex-sm-row flex-column align-items-sm-center align-items-start text-secondary fw-light mb-1 ">
           <span>
-            $12.99<i class="bi bi-x"></i>Unidad
+            ${price}
+            <i class="bi bi-x"></i>Unidad
           </span>
+          <i class="bi bi-dot d-none d-sm-flex"></i>
+          <span>{stock} En Stock</span>
         </div>
         <div className="d-flex flex-row align-items-center">
           <div>
-            <div className="d-flex flex-row align-items-center rounded-pill border px-2 w-50">
-              <button className="text-secondary fs-5 me-1 border-0 rounded-pill bg-white">
+            <div className="d-flex flex-row align-items-center rounded-pill border px-2 w-75">
+              <button
+                onClick={() => handleIncrement()}
+                className="text-secondary fs-5 me-1 border-0 rounded-pill bg-white"
+              >
                 +
               </button>
               <input
-                className=" text-center w-50 border-0 border-start-0 border-1 border-secondary"
+                className=" text-center w-75 border-0 border-start-0 border-1 border-secondary"
                 type="number"
-                value={1}
+                value={mount}
               />
-              <button className="text-secondary fs-5 ms-1 border-0 rounded-pill bg-white">
+              <button
+                onClick={() => handleDecrement()}
+                className="text-secondary fs-5 ms-1 border-0 rounded-pill bg-white"
+              >
                 -
               </button>
             </div>
