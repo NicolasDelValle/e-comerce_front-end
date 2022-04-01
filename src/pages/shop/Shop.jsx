94d8377e1c250b1b3fd0d0c4e27acc387fc315e8
Charlygaf <charlygaf@gmail.com>
2022-04-01 13:48:19 -0300

    shop

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,5 +1,5 @@
 import "./shopStyles.css";
-import { Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import NavigationBar from "../../components/NavigationBar";
 import Footer from "../../components/Footer";
@@ -9,8 +9,16 @@ import axios from "axios";
 
 function Shop(props) {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
+    const getCategories = async () => {
+      const { data } = await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}categories`,
+      });
+      setCategories(data);
+    };
     const getProducts = async () => {
       const { data } = await axios({
         method: "get",
@@ -18,41 +26,27 @@ function Shop(props) {
       });
       setProducts(data);
     };
-
+    getCategories();
     getProducts();
+    console.log(categories);
+    console.log(products);
   }, []);
-
-  // for (const key in products) {
-  //   if (key.categoryId === 1) {
-  //     setTablas(key);
-  //   }
-  //   if (key.categoryId === 2) {
-  //     setRuedas(key);
-  //   }
-  //   if (key.categoryId === 3) {
-  //     setHardware(key);
-  //   } else {
-  //     setRopa(key);
-  //   }
-  // }
 
   return (
     <>
       <NavigationBar className="bg-dark" />
       <div className="section">
         <div className="container">
-          <div id="Tablas" className="row m-3 g-3">
-            <h2 className="mt-4">Tablas</h2>
-            <Row className="py-5 justify-content-center ">
-              {/* <ProductCard key={i} product={product} /> */}
-            </Row>
-          </div>
-          <div id="Hardware" className="row m-3 g-3">
-            <h2 className="mt-4">Hardware</h2>
-          </div>
-          <div id="Ropa" className="row m-3 g-3">
-            <h2 className="mt-4">Ropa</h2>
-          </div>
+          {categories.map((category) => (
+            <div className="row m-3 g-3">
+              <h2 className="mt-4">{category.name}</h2>
+              {products
+                .filter((product) => product.categoryId === category.id)
+                .map((filteredProduct) => (
+                  <ProductCard product={filteredProduct} />
+                ))}{" "}
+            </div>
+          ))}
         </div>
       </div>
       <Footer />
