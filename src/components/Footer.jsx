f7ec5b80f2e32bd835c92e6ad0a7cf7b05a11e6d
Luis Zunino <zuninoluis01@gmail.com>
2022-04-01 15:08:09 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,43 +4,44 @@ import "./css/Footer.css";
 function Footer() {
   return (
     <div className="colorFooter text-white mb-0 mt-auto pt-4">
-      <div className="footer container py-2">
-        <div className="row text-start px-0 text-center">
-          <Link to={"/"} className="col-sm-3 text-white m-0">
-            Home
-          </Link>
-          <Link to={"/shop"} className="col-sm-3 text-white m-0">
-            Products
-          </Link>
-          <Link to={"/"} className="col-sm-3 text-white m-0">
-            About Project
-          </Link>
-          <Link to={"/aboutus"} className="col-sm-3 text-white m-0">
-            About Us
-          </Link>
-        </div>
-        <div className="d-flex justify-content-between align-items-center p-2 mx-auto">
-          <i className="align-item-center">
-            <div className="w-55 mx-auto ">
-              <hr />
+      <div className="footer">
+        <div className="container">
+          <div className="row py-2">
+            <div className="col-md-3 col-6 text-start">
+              <h6 className="mb-3">Contacto</h6>
+              <p>contacto@contacto.uy</p>
+              <p>091 234 567</p>
+              <p>Política de privacidad</p>
             </div>
-            <p>
-              Este proyecto consiste en la creación de un sitio web, donde se
-              volcaron todos los conocimientos obtenidos durante la cursada,
-              reflejandolos en las características y funcionalidades que se
-              esperan encontrar a diario en un e-commerce.
-            </p>
-            <div className="w-55 mx-auto ">
-              <hr />
+            <div className="col-md-3 col-6 text-start">
+              <h6 className="mb-3">Tienda</h6>
+              <div className="text-start text-start">
+                <Link to={"/"} className="d-block text-white mb-3">
+                  Home
+                </Link>
+                <Link to={"/shop"} className="d-block text-white mb-3">
+                  Products
+                </Link>
+                <Link to={"/aboutproject"} className="d-block text-white mb-3">
+                  About Project
+                </Link>
+              </div>
             </div>
-          </i>
-        </div>
-
-        <div className="row text-center  pt-2">
-          <div className="col-sm-6">
-            <p className="">LOSI SKATE CO.</p>
+            <div className="col-md-6 col d-flex align-items-center">
+              <p>
+                Este proyecto consiste en la creación de un sitio web, donde se
+                volcaron todos los conocimientos obtenidos durante la cursada,
+                reflejandolos en las características y funcionalidades que se
+                esperan encontrar a diario en un e-commerce.
+              </p>
+            </div>
           </div>
-          <div className="col-sm-6">
+        </div>
+        <hr />
+        <div className="container">
+          <div className="d-flex justify-content-between  pt-2">
+            <p className="">LOSI SKATE CO.</p>
+
             <p className="mb-0 fs-6">All rights reserved</p>
           </div>
         </div>
