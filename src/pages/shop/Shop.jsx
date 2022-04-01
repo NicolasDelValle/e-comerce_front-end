fa64164c0381042b2f7f73e70f232158e7f8ec16
Charlygaf <charlygaf@gmail.com>
2022-04-01 09:26:20 -0300

    shop, foto de perfil

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,13 +1,41 @@
 import "./shopStyles.css";
-import React from "react";
-import tabla from "../../img/productos/tablas/tabla3color.png";
-import hardware from "../../img/productos/hardware/bronsonbearing_720x@2x.png";
-import ropa from "../../img/productos/ropa/pant_marron_1.png";
+import { Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 import NavigationBar from "../../components/NavigationBar";
 import Footer from "../../components/Footer";
 import AddCart from "../../components/AddCart";
+import ProductCard from "../../components/ProductCard";
+import axios from "axios";
 
 function Shop(props) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const { data } = await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}products`,
+      });
+      setProducts(data);
+    };
+
+    getProducts();
+  }, []);
+
+  // for (const key in products) {
+  //   if (key.categoryId === 1) {
+  //     setTablas(key);
+  //   }
+  //   if (key.categoryId === 2) {
+  //     setRuedas(key);
+  //   }
+  //   if (key.categoryId === 3) {
+  //     setHardware(key);
+  //   } else {
+  //     setRopa(key);
+  //   }
+  // }
+
   return (
     <>
       <NavigationBar className="bg-dark" />
@@ -15,69 +43,15 @@ function Shop(props) {
         <div className="container">
           <div id="Tablas" className="row m-3 g-3">
             <h2 className="mt-4">Tablas</h2>
-
-            <div className="col-md-4 h-75 rounded">
-              {<img src={`${tabla}`} alt="tabla" />}
-
-              <p>Nombre: Producto</p>
-              <p>$ 800</p>
-              <AddCart />
-            </div>
-            <div className="col-md-4 rounded">
-              {<img src={`${tabla}`} alt="tabla" />}
-              <p>Nombre: Producto</p>
-              <p>$ 800</p>
-              <AddCart />
-            </div>
-
-            <div className="col-md-4 rounded">
-              {<img src={`${tabla}`} alt="tabla" />}
-              <p>Nombre: Producto</p>
-              <p>$ 800</p>
-              <AddCart />
-            </div>
+            <Row className="py-5 justify-content-center ">
+              {/* <ProductCard key={i} product={product} /> */}
+            </Row>
           </div>
           <div id="Hardware" className="row m-3 g-3">
             <h2 className="mt-4">Hardware</h2>
-            <div className="col-md-4 rounded">
-              {<img src={`${hardware}`} alt="tabla" />}
-              <p>Nombre: Producto</p>
-              <p>$ 800</p>
-              <AddCart />
-            </div>
-            <div className="col-md-4 rounded">
-              {<img src={`${hardware}`} alt="tabla" />}
-              <p>Nombre: Producto</p>
-              <p>$ 800</p>
-              <AddCart />
-            </div>
-            <div className="col-md-4 rounded">
-              {<img src={`${hardware}`} alt="tabla" />}
-              <p>Nombre: Producto</p>
-              <p>$ 800</p>
-              <AddCart />
-            </div>
           </div>
           <div id="Ropa" className="row m-3 g-3">
             <h2 className="mt-4">Ropa</h2>
-            <div className="col-md-4 rounded">
-              {<img src={`${ropa}`} alt="tabla" />}
-              <p>Nombre: Producto</p>
-              <p>$ 800</p>
-              <AddCart />
-            </div>
-            <div className="col-md-4 rounded">
-              {<img src={`${ropa}`} alt="tabla" />}
-              <p>Nombre: Producto</p>
-              <p>$ 800</p>
-              <AddCart />
-            </div>
-            <div className="col-md-4 rounded">
-              {<img src={`${ropa}`} alt="tabla" />}
-              <p>Nombre: Producto</p>
-              <p>$ 800</p>
-              <AddCart />
-            </div>
           </div>
         </div>
       </div>
