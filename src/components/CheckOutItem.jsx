e75873b18b22637158df40dbbb5d2888797d2aae
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-04-03 21:44:29 -0300

    checkoutitem

---
+++ src/components/CheckOutItem.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import actions from "../redux/actions/cartActions";
+
+export const CheckOutItem = ({
+  url,
+  price,
+  mountProp,
+  name,
+  stock,
+  productId,
+}) => {
+  return (
+    <div className="d-flex flex-row align-items-center w-100 mb-3">
+      <div className="w-100 me-3 ps-2">
+        <div className="w-100 d-flex flex-row align-items-start mb-1">
+          <span className="w-75 fs-6 fw-light text-start overflow-hidden">
+            <span className="fw-bold me-1">{mountProp}x</span>
+            {name}
+          </span>
+          <span className="ms-auto fs-6 fw-bold fw-light">
+            ${Number(price) * Number(mountProp)}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckOutItem;
