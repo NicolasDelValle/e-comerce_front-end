a438eb0c6f4096bdc509d0340d27d4e3cf276716
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-04-08 11:24:29 -0300

    Historial de ordenes

--- src/components/OrderListItem.jsx
+++ src/components/OrderListItem.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { Accordion } from "react-bootstrap";
-export const OrderListItem = ({ id, date, total, address, productList }) => {
+export const OrderListItem = ({
+  id,
+  date,
+  total,
+  address,
+  productList,
+  status,
+}) => {
+  const iva = Number(total) * 0.21;
+  const subTotal = Number(total) - iva;
   return (
     <>
       <Accordion>
@@ -10,45 +19,71 @@ export const OrderListItem = ({ id, date, total, address, productList }) => {
               <div className="w-100 d-flex align-items-center ">
                 <span className="">{date}</span>
 
-                <span className="mx-auto fw-light text-secondary--">
+                <span className="mx-auto fw-light text-secondary d-none d-lg-flex">
                   {address}
                 </span>
-                <span className=" fs-4 fw-bold  me-3">${total}</span>
+                <span className=" fs-4 fw-bold ms-auto ms-lg-1  me-3">
+                  ${total}
+                </span>
               </div>
               <div className=" d-flex align-items-center ">
-                <span className="me-auto badge rounded-pill bg-success">
-                  Recibido
-                </span>
+                {status === "Procesando" ? (
+                  <span className="me-auto badge rounded-pill bg-secondary">
+                    {status}
+                  </span>
+                ) : (
+                  <></>
+                )}
+                {status === "Enviado" ? (
+                  <span className="me-auto badge rounded-pill bg-warning">
+                    {status}
+                  </span>
+                ) : (
+                  <></>
+                )}
+                {status === "Recibido" ? (
+                  <span className="me-auto badge rounded-pill bg-success">
+                    {status}
+                  </span>
+                ) : (
+                  <></>
+                )}
+
                 <span className="me-3 fw-light text-secondary">id: {id}</span>
               </div>
             </div>
           </Accordion.Header>
           <Accordion.Body>
             <div className="d-flex flex-column">
+              <span className="me-auto fw-bold d-flex d-lg-none">
+                Direccion:
+              </span>
+              <span className="text-start me-auto mb-2 text-secondary d-flex d-lg-none">
+                {address}
+              </span>
+
               <div className="d-flex flex-column bg-light ps-1 py-1 border-start border-4 border-dark">
-                <div className="w-100 d-flex">
-                  <span className="fs-6 fw-bold me-1">x4</span>
-                  <span className="fs-6">Tabla re linda </span>
-                  <span className="fs-6 fw-bold ms-auto">$5000</span>
-                </div>
-                <div className="w-100 d-flex">
-                  <span className="fs-6 fw-bold me-1">x4</span>
-                  <span className="fs-6">Tabla re linda </span>
-                  <span className="fs-6 fw-bold ms-auto">$5000</span>
-                </div>
-                <div className="w-100 d-flex">
-                  <span className="fs-6 fw-bold me-1">x4</span>
-                  <span className="fs-6">Tabla re linda </span>
-                  <span className="fs-6 fw-bold ms-auto">$5000</span>
-                </div>
+                {productList.map((product) => (
+                  <div className="w-100 d-flex">
+                    <span className="fs-6 fw-bold me-1">
+                      x{product.quantity}
+                    </span>
+                    <span className="fs-6">{product.name}</span>
+                    <span className="fs-6 fw-bold ms-auto">
+                      ${product.price}
+                    </span>
+                  </div>
+                ))}
               </div>
               <div className="w-100 d-flex mt-1">
                 <span className="fs-5 fw-light">IVA</span>
-                <span className="fs-5 fw-bold ms-auto">$500</span>
+                <span className="fs-5 fw-bold ms-auto">${iva}</span>
               </div>
               <div className="w-100 d-flex">
                 <span className="fs-5 fw-light">Subtotal</span>
-                <span className="fs-5 fw-bold ms-auto">$5000</span>
+                <span className="fs-5 fw-bold ms-auto">
+                  ${Math.ceil(subTotal)}
+                </span>
               </div>
             </div>
           </Accordion.Body>
--- src/pages/orderHistory/UserOrderHistory.jsx
+++ src/pages/orderHistory/UserOrderHistory.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import NavigationBar from "../../components/NavigationBar";
-
+import OrderListItem from "../../components/OrderListItem";
 import { Col, Container, Row, Button, Accordion } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -11,26 +11,35 @@ export const UserOrderHistory = (props) => {
 
   const handleGetOrders = async () => {
     const orders = await getUserOrders(user.newToken);
-    /* const a = JSON.parse(orders.data[0].productList); */
-    setOrders(orders);
+
+    setOrders(orders.data);
   };
 
   useEffect(() => {
     handleGetOrders();
   }, []);
-  const time = new Date(orders?.data[0].createdAt).toLocaleDateString();
   console.log(orders);
   return (
     <>
       <NavigationBar />
       <Container className="mt-3 px-lg-5">
-        <Button onClick={() => handleGetOrders()}>a</Button>
         <div>
           <span>Total de cuenta</span>
           <span>$44022</span>
         </div>
         <span className="w-100 me-auto d-flex fs-4 fw-light">Historial</span>
-        <div></div>
+        <div>
+          {orders?.map((order) => (
+            <OrderListItem
+              id={order.id}
+              date={new Date(order.createdAt).toLocaleDateString()}
+              total={order.totalPrice}
+              address={order.address}
+              productList={JSON.parse(order.productList)}
+              status={order.status}
+            />
+          ))}
+        </div>
       </Container>
     </>
   );
