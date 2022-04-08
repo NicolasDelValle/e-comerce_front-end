6de3f45c8549fd6ec47fe61bd3c5ca7aed79ddaa
Charlygaf <charlygaf@gmail.com>
2022-04-08 18:02:11 -0300

    cart random products

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -7,95 +7,124 @@ import CartItem from "../../components/CartItem";
 import Footer from "../../components/Footer";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import "./cart.css";
+import ProductCard from "../../components/ProductCard";
 
 function Cart() {
-	const { cart, user } = useSelector((state) => state);
-	const SubTotalCost = cart.reduce(function (prev, product) {
-		return prev + Number(product.price) * product.quantity;
-	}, 0);
+  const [products, setProducts] = useState([]);
+  const randomProducts = products;
+  const { cart, user } = useSelector((state) => state);
+  const SubTotalCost = cart.reduce(function (prev, product) {
+    return prev + Number(product.price) * product.quantity;
+  }, 0);
 
-	const total = SubTotalCost * 1.21;
+  const total = SubTotalCost * 1.21;
 
-	return (
-		<>
-			<NavigationBar />
-			<Container className="mt-5">
-				{cart.length !== 0 ? (
-					<Row className="d-flex justify-content-center">
-						<Col md={12} lg={6} className="pe-lg-0">
-							{cart.map((product, i) => (
-								<CartItem
-									key={i}
-									url={product.imageUrl}
-									price={product.price}
-									mountProp={product.quantity}
-									name={product.name}
-									stock={product.stock}
-									productId={product.id}
-								/>
-							))}
-						</Col>
-						<Col md={12} lg={4}>
-							<div className="border p-3 mb-3">
-								<div className="border-bottom d-flex align-items-start mb-2 fs-5 fw-bold">
-									<span>TOTAL DEL CARRITO</span>
-								</div>
-								<div className="d-flex align-items-start mb-2">
-									<span>Subtotal</span>
-									<span className="fs-4 ms-auto">{SubTotalCost}</span>
-								</div>
-								<div className="d-flex align-items-start mb-2">
-									<span>Total</span>
-									<span className="fs-4 ms-auto">{total}</span>
-								</div>
-								<div>
-									<Link to={user.newToken ? "/cart/checkout" : "/login"}>
-										<Button
-											className="rounded-pill w-100 px-auto py-1 text-decoration-none"
-											variant="dark"
-											size="lg"
-										>
-											Finalizar Compra
-										</Button>
-									</Link>
-								</div>
-								<div className="d-flex pt-2 justify-content-between">
-									<div className=" align-items-center justify-content-end">
-										<FcLock size={25} />
-									</div>
-									<div className=" align-items-center justify-content-end">
-										<FaCcVisa size={30} className="ms-1" />
-										<FaCcMastercard size={30} className="ms-1" />
-										<FaCcPaypal size={30} className="ms-1" />
-									</div>
-								</div>
-							</div>
-						</Col>
-					</Row>
-				) : (
-					<div
-						className="container d-flex flex-column justify-content-center align-items-center"
-						style={{ height: "50vh" }}
-					>
-						<div className="m-auto">
-							<BsCart3 size={60} />
-							<h1 className="fs-2 mt-2 my-5">
-								No has agregado artículos al carrito aún.
-							</h1>
-							<Link to={"/shop"} className="text-white text-decoration-none">
-								<Button className="rounded-pill bg-black" size="md" variant="dark">
-									Volver a la tienda
-								</Button>
-							</Link>
-						</div>
-					</div>
-				)}
-			</Container>
-			<Footer />
-		</>
-	);
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const { data } = await axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}products`,
+    });
+
+    setProducts(data);
+  };
+
+  return (
+    <>
+      <NavigationBar />
+      <Container className="mt-5">
+        {cart.length !== 0 ? (
+          <Row className="d-flex justify-content-center">
+            <Col md={12} lg={6} className="pe-lg-0">
+              {cart.map((product, i) => (
+                <CartItem
+                  key={i}
+                  url={product.imageUrl}
+                  price={product.price}
+                  mountProp={product.quantity}
+                  name={product.name}
+                  stock={product.stock}
+                  productId={product.id}
+                />
+              ))}
+            </Col>
+            <Col md={12} lg={4}>
+              <div className="border p-3 mb-3">
+                <div className="border-bottom d-flex align-items-start mb-2 fs-5 fw-bold">
+                  <span>TOTAL DEL CARRITO</span>
+                </div>
+                <div className="d-flex align-items-start mb-2">
+                  <span>Subtotal</span>
+                  <span className="fs-4 ms-auto">{SubTotalCost}</span>
+                </div>
+                <div className="d-flex align-items-start mb-2">
+                  <span>Total</span>
+                  <span className="fs-4 ms-auto">{total}</span>
+                </div>
+                <div>
+                  <Link to={user.newToken ? "/cart/checkout" : "/login"}>
+                    <Button
+                      className="rounded-pill w-100 px-auto py-1 text-decoration-none"
+                      variant="dark"
+                      size="lg"
+                    >
+                      Finalizar Compra
+                    </Button>
+                  </Link>
+                </div>
+                <div className="d-flex pt-2 justify-content-between">
+                  <div className=" align-items-center justify-content-end">
+                    <FcLock size={25} />
+                  </div>
+                  <div className=" align-items-center justify-content-end">
+                    <FaCcVisa size={30} className="ms-1" />
+                    <FaCcMastercard size={30} className="ms-1" />
+                    <FaCcPaypal size={30} className="ms-1" />
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        ) : (
+          <div
+            className="container d-flex flex-column justify-content-center align-items-center"
+            style={{ height: "50vh" }}
+          >
+            <div className="m-auto">
+              <BsCart3 size={60} />
+              <h1 className="fs-2 mt-2 my-5">
+                No has agregado artículos al carrito aún.
+              </h1>
+              <Link to={"/shop"} className="text-white text-decoration-none">
+                <Button
+                  className="rounded-pill bg-black"
+                  size="md"
+                  variant="dark"
+                >
+                  Volver a la tienda
+                </Button>
+              </Link>
+            </div>
+          </div>
+        )}
+        <Row className="  justify-content-center g-5 my-5  border-top py-1">
+          {randomProducts
+            .filter((product) => product.id < 5)
+            .map((product, i) => (
+              <ProductCard key={i} product={product} />
+            ))}
+        </Row>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default Cart;
