20e7f6a86ebe5f7c881ccc36dccb1c290541bc6a
mauroraviolo23 <mauro.raviolo@gmail.com>
2022-04-08 17:41:08 -0300

    col de home

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,50 +5,50 @@ import DetailsButton from "./DetailsButton";
 import "./css/ProductCard.css";
 
 function ProductCard({ product }) {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <>
-      <Col md={5} lg={3} className="productCard rounded pt-1 pb-3">
-        <Link to={`/products/${product.slug}`}>
-          <div className=" p-1 text-black">
-            <span className="py-2" min-height="220px">
-              {isLoading && (
-                <div
-                  className="d-flex justify-content-center align-items-center"
-                  style={{ height: "220px" }}
-                >
-                  <Spinner animation="border" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                  </Spinner>
-                </div>
-              )}
-              <img
-                className={isLoading ? "d-none" : "w-auto mb-2"}
-                height="220px"
-                // agregar supabaseurl para imagenes de la DB
-                // src={product && `https://cybmmybwfbmxasdepuyj.supabase.co/storage/v1/object/public/images/${product.imageUrl}`}
-                src={product.imageUrl}
-                alt={product.name}
-                onLoad={() => setIsLoading(false)}
-              />
-              <p className="product-info mt-2 text-center">
-                <strong>{product.name}</strong>
-              </p>
-              <p className="product-info  text-center">
-                <strong>${product.price}</strong>
-              </p>
-            </span>
-            <div className="d-flex justify-content-center mt-2">
-              <DetailsButton />
-            </div>
-          </div>
-        </Link>
-      </Col>
-    </>
-  );
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+	return (
+		<>
+			<Col md={6} lg={3} className="productCard rounded pt-1 pb-3">
+				<Link to={`/products/${product.slug}`}>
+					<div className=" p-1 text-black">
+						<span className="py-2" min-height="220px">
+							{isLoading && (
+								<div
+									className="d-flex justify-content-center align-items-center"
+									style={{ height: "220px" }}
+								>
+									<Spinner animation="border" role="status">
+										<span className="visually-hidden">Loading...</span>
+									</Spinner>
+								</div>
+							)}
+							<img
+								className={isLoading ? "d-none" : "w-auto mb-2"}
+								height="220px"
+								// agregar supabaseurl para imagenes de la DB
+								// src={product && `https://cybmmybwfbmxasdepuyj.supabase.co/storage/v1/object/public/images/${product.imageUrl}`}
+								src={product.imageUrl}
+								alt={product.name}
+								onLoad={() => setIsLoading(false)}
+							/>
+							<p className="product-info mt-2 text-center">
+								<strong>{product.name}</strong>
+							</p>
+							<p className="product-info  text-center">
+								<strong>${product.price}</strong>
+							</p>
+						</span>
+						<div className="d-flex justify-content-center mt-2">
+							<DetailsButton />
+						</div>
+					</div>
+				</Link>
+			</Col>
+		</>
+	);
 }
 
 export default ProductCard;
